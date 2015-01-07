b8e4ee6c84b4b0f387deae5ed0d72ef639abc8df
AppleLAN <alanbuscaglia@icloud.com>
2015-01-07 09:44:57 -0300

    New Design
    Bootstrap

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+$(function () {
+
+
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('#selectAll').click(function(){
+    if($(this).prop("checked")){
+      $('#tasks input').attr("checked" ,"checked");
+      $('$tasks input').addClass('done');
+      $('$tasks input span').addClass('ng-binding done');
+    }
+    else{
+      $('#tasks input').removeProp("checked");
+      $('$tasks input span').removeClass('done');
+    }
+  });
+  $('#openConfig').click(function(e){
+      e.preventDefault();
+      $('#configurationPanel').modal();
+  })
+
+
+})
